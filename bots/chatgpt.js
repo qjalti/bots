43cb0c59119a74420b1612100f0a52ed4201126c
qjalti <qjalti@gmail.com>
2023-06-04 22:00:49 +0300

    errors handle, JSDoc comments add

--- bots/chatgpt.js
+++ bots/chatgpt.js
@@ -12,19 +12,41 @@ import {
 } from '../src/constants.js';
 import * as dotenv from 'dotenv';
 
-dotenv.config();
-
+/**
+ * Constants definations
+ */
 const INITIAL_SESSION = {
   messages: [],
 };
+const SIXTEEN_DASHES = `----------------\n`;
 
+
+/**
+ * Modules configurations
+ */
+dotenv.config();
 const BOT = new Telegraf(process.env.TELEGRAM_TOKEN);
 BOT.use(session());
 
-const SIXTEEN_DASHES = `----------------\n`;
+/**
+ * Custom functions
+ */
 
+/**
+ * Log message by send it to logs group
+ * @param {string} message
+ * @return {Promise<void>}
+ */
 const logMessage = async (message) => {
   await BOT.telegram.sendMessage(LOGS_CHAT_ID, message);
+};
+
+/**
+ * Clear session data
+ * @param {Object} ctx Context
+ */
+const sessionClear = (ctx) => {
+  ctx.session = INITIAL_SESSION;
 };
 
 /**
@@ -35,41 +57,46 @@ BOT.catch(async (error, ctx) => {
   await ctx.reply(ERROR_MESSAGE);
 });
 
+/**
+ * Commands handler
+ */
 BOT.command('start', async (ctx) => {
   ctx.session = INITIAL_SESSION;
 });
-
 BOT.command('new', async (ctx) => {
   ctx.session = INITIAL_SESSION;
   await ctx.reply('История переписки сброшена');
 });
-
 BOT.command('examples', async (ctx) => {
   await ctx.reply(CHAT_GPT_EXAMPLES);
 });
-
 BOT.command('my_id', async (ctx) => {
   await ctx.replyWithMarkdownV2(`\`${ctx.chat.id}\``);
 });
-
 BOT.command('author', async (ctx) => {
   await ctx.reply(AUTHOR_COMMAND);
   await ctx.sendContact('+79883857654', 'Никита');
 });
 
 /**
- * Блок обработки войсов
+ * On voice only sent
  */
 BOT.on(message('voice'), async (ctx) => {
   let log = SIXTEEN_DASHES;
+
   const USER_DATA = ctx.chat.id + ', ' + ctx.chat.username;
   const USER_ID = ctx.chat.id;
+
   log += USER_DATA + `\n`;
   log += SIXTEEN_DASHES;
+
   const LINK = await ctx.telegram.getFileLink(ctx.message.voice.file_id);
+
   ctx.session ??= INITIAL_SESSION;
+
   await ctx.telegram.sendChatAction(ctx.chat.id, 'choose_sticker');
   let stickerMessageId = null;
+
   setTimeout(async () => {
     await ctx.replyWithAnimation(
         ANIMATED_STICKERS[Math.floor(
@@ -85,35 +112,38 @@ BOT.on(message('voice'), async (ctx) => {
     const OGG_PATH = await OGG.create(LINK.href, USER_ID_OGG);
     const MP3_PATH = await OGG.toMP3(OGG_PATH, USER_ID_OGG);
     const TEXT = await OPEN_AI.transcription(MP3_PATH);
+
     log += LINK + `\n` + SIXTEEN_DASHES;
     log += TEXT.data + `\n` + SIXTEEN_DASHES;
+
     await ctx.reply(`Я так понял Вы сказали:\n"${TEXT.data}"`);
+
     ctx.session.messages.push(
         {
           role: OPEN_AI.roles.USER, content: TEXT.data,
         },
     );
+
     const RESPONSE = await OPEN_AI.chat(ctx.session.messages);
+
+    if (RESPONSE.code === 4) {
+      sessionClear(ctx);
+    }
+
     ctx.session.messages.push(
         {
-          role: OPEN_AI.roles.ASSISTANT, content: RESPONSE.content,
+          role: OPEN_AI.roles.ASSISTANT, content: RESPONSE.data.content,
         },
     );
-    log += RESPONSE.content + `\n` + SIXTEEN_DASHES;
 
-    if (!TEXT.success && !RESPONSE.content) {
-      await ctx.reply(ERROR_MESSAGE);
-      await ctx.telegram.sendMessage(
-          AUTHOR_TELEGRAM_ID,
-          `У одного из пользователей произошла ошибка. Смотри логи`,
-      );
-      await ctx.telegram.sendMessage(
-          LOGS_CHAT_ID,
-          JSON.stringify(TEXT, null, 2),
-      );
-    } else {
-      await ctx.replyWithMarkdown(RESPONSE.content);
+    log += RESPONSE.data.content + `\n` + SIXTEEN_DASHES;
+
+    if (RESPONSE.success) {
+      await ctx.replyWithMarkdown(RESPONSE.data.content);
+    } else if (RESPONSE.code === 4) {
+      sessionClear(ctx);
     }
+
     if (USER_ID !== AUTHOR_TELEGRAM_ID) {
       await logMessage(log);
     }
@@ -125,19 +155,28 @@ BOT.on(message('voice'), async (ctx) => {
       await ctx.telegram.deleteMessage(ctx.chat.id, stickerMessageId);
     }
     ctx.reply(ERROR_MESSAGE);
-    console.log('Error while voice message sent. ', err.message, err);
+    console.log('Error while voice message send: ', err.message, err);
   }
 });
 
+/**
+ * On text only send
+ */
 BOT.on(message('text'), async (ctx) => {
   let log = SIXTEEN_DASHES;
+
   const USER_DATA = ctx.chat.id + ', ' + ctx.chat.username;
   const USER_ID = ctx.chat.id;
+
   log += USER_DATA + `\n`;
   log += SIXTEEN_DASHES;
+
   ctx.session ??= INITIAL_SESSION;
+
   await ctx.telegram.sendChatAction(ctx.chat.id, 'choose_sticker');
+
   let stickerMessageId = null;
+
   setTimeout(async () => {
     await ctx.replyWithAnimation(
         ANIMATED_STICKERS[Math.floor(
@@ -148,8 +187,10 @@ BOT.on(message('text'), async (ctx) => {
       stickerMessageId = res.message_id;
     });
   }, 1000);
+
   try {
     log += ctx.message.text + `\n` + SIXTEEN_DASHES;
+
     ctx.session.messages.push(
         {
           role: OPEN_AI.roles.USER, content: ctx.message.text,
@@ -159,12 +200,18 @@ BOT.on(message('text'), async (ctx) => {
 
     ctx.session.messages.push(
         {
-          role: OPEN_AI.roles.ASSISTANT, content: RESPONSE.content,
+          role: OPEN_AI.roles.ASSISTANT, content: RESPONSE.data.content,
         },
     );
-    log += RESPONSE.content + `\n` + SIXTEEN_DASHES;
 
-    await ctx.replyWithMarkdown(RESPONSE.content);
+    log += RESPONSE.data.content + `\n` + SIXTEEN_DASHES;
+
+    if (RESPONSE.success) {
+      await ctx.replyWithMarkdown(RESPONSE.data.content);
+    } else if (RESPONSE.code === 4) {
+      sessionClear(ctx);
+    }
+
     if (USER_ID !== AUTHOR_TELEGRAM_ID) {
       await logMessage(log);
     }
@@ -176,7 +223,7 @@ BOT.on(message('text'), async (ctx) => {
       await ctx.telegram.deleteMessage(ctx.chat.id, stickerMessageId);
     }
     ctx.reply(ERROR_MESSAGE);
-    console.log('Error while text message sent. ', err.message, err);
+    console.log('Error while text message send: ', err.message, err);
   }
 });
 
--- src/openai.js
+++ src/openai.js
@@ -3,8 +3,14 @@ import {createReadStream} from 'fs';
 import {ERROR_MESSAGE} from './constants.js';
 import * as dotenv from 'dotenv';
 
+/**
+ * Modules settings
+ */
 dotenv.config();
 
+/**
+ * Send requests to OpenAI API
+ */
 class OpenAI {
   roles = {
     ASSISTANT: 'assistant',
@@ -12,6 +18,10 @@ class OpenAI {
     SYSTEM: 'system',
   };
 
+  /**
+   * Class init
+   * @param {string} apiKey Open AI API token
+   */
   constructor(apiKey) {
     const CONFIGURATION = new Configuration({
       apiKey,
@@ -19,22 +29,38 @@ class OpenAI {
     this.OPEN_AI = new OpenAIApi(CONFIGURATION);
   }
 
+  /**
+   * Requst to Open AI chat model
+   * @param {Object} messages Object with messages history between user and chat
+   * @return {Promise<{success: boolean, content: string}|*>}
+   */
   async chat(messages) {
     try {
       const RESPONSE = await this.OPEN_AI.createChatCompletion({
         model: 'gpt-3.5-turbo',
         messages,
       });
-      return RESPONSE.data.choices[0].message;
+      return {
+        success: true,
+        code: 0,
+        data: RESPONSE.data.choices[0].message,
+      };
     } catch (err) {
-      console.log('Error! While trying GPT-chat', err.message, err);
+      console.log('Error in chat() method', err.message);
+      console.log(err, 'err');
       return {
         success: false,
-        content: ERROR_MESSAGE,
+        code: 0,
+        data: ERROR_MESSAGE,
       };
     }
   }
 
+  /**
+   * Voice-to-text (transcription)
+   * @param {string} filepath Path to voice .mp3 file
+   * @return {Promise<{success: boolean, content: string}|{data, success: boolean}>}
+   */
   async transcription(filepath) {
     try {
       const REPONSE = await this.OPEN_AI.createTranscription(
@@ -46,7 +72,7 @@ class OpenAI {
         data: REPONSE.data.text,
       };
     } catch (err) {
-      console.log('Error while trying transcription! ', err.message, err);
+      console.log('Error in transcription() method', err.message, err);
       return {
         success: false,
         content: ERROR_MESSAGE,
